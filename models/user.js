d50b88f8ffd523253c4a48d46a46f2a8ffb3ea6c
Hiram Cruz <hiram0101@gmail.com>
2018-05-24 11:22:25 -0400

    Added user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+userSchema.set('toObject', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+    delete ret._it
+    delete ret.__v
+    delete ret.password
+  }
+})
+
+userSchema.statics.hasPassword = password => {
+  return bcrypt.hash(password, 10)
+}
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
