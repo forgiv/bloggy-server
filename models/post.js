0fcab71a780004d646a0394dc152197103756e35
Hiram Cruz <hiram0101@gmail.com>
2018-05-24 11:37:57 -0400

    Added post model

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const postSchema = mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  title: { type: String, required: true },
+  content: String
+}, { timestamps: true })
+
+postSchema.index({ title: 1, userId: 1 }, { unique: true })
+
+postSchema.set('toObject', {
+  transform: function(doc, ret) {
+    ret.id = ret._id
+    delete ret._id
+    delete ret.__v
+  }
+})
+
+module.exports = mongoose.model('Post', postSchema)
