25944e6f1de59e77d0e8ad17d847f2166fba97e8
Hiram Cruz <hiram0101@gmail.com>
2018-05-24 11:08:56 -0400

    Added code to start server Added code for passport authentication Added config Add .env to .gitignore

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports.PORT = process.env.PORT || 8080
+module.exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/bloggy'
+module.exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/bloggy-test'
+module.exports.JWT_SECRET = process.env.JWT_SECRET
+module.exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'
---
+++ server.js
@@ -0,0 +1,82 @@
+require("dotenv").config()
+
+const express = require("express")
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+const passport = require('passport')
+
+const { PORT, MONGODB_URI } = require("./config")
+const localStrategy = require("./passport/local")
+const jwtStrategy = require('./passport/jwt')
+
+// const authRouter = require('./routers/auth')
+// const userRouter = require("./routers/users")
+// const blogRouter = require('./routers/blogs')
+// const postRouter = require('./routers/posts')
+// const commentRouter = require('./routers/comments')
+// const rssRouter = require('./routers/rss')
+// const dashboardRouter = require('./routers/dashboard')
+
+passport.use(localStrategy)
+passport.use(jwtStrategy)
+
+const app = express()
+
+// More verbose logging while in development
+// Skip logging when testing
+app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
+  skip: () => process.env.NODE_ENV === 'test'
+}))
+// Parse json in request
+app.use(express.json())
+
+// Mount routers
+// app.use('/api', authRouter)
+// app.use('/api/users', userRouter)
+// app.use('/api/blogs', blogRouter)
+// app.use('/api/posts', postRouter)
+// app.use('/api/comments', commentRouter)
+// app.use('/api/rss', rssRouter)
+// app.use('/api/dashboard', dashboardRouter)
+
+// Catch-all 404
+app.use(function(req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  })
+})
+
+
+if (require.main === module) {
+  mongoose
+    .connect(MONGODB_URI)
+    .then(instance => {
+      const conn = instance.connections[0];
+      console.info(
+        `Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`
+      );
+    })
+    .catch(err => {
+      console.error(`ERROR: ${err.message}`);
+      console.error("\n === Did you remember to start `mongod`? === \n");
+      console.error(err);
+    });
+
+  app.listen(PORT, function() {
+    console.info(`Server listening on ${this.address().port}`)
+  }).on('error', err => {
+    console.error(err)
+  })
+}
+
+module.exports = app
