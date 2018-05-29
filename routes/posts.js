3ac61a5eacf79b1075349a8c5b27d28df721c7c0
Hiram Cruz <hiram0101@gmail.com>
2018-05-29 10:57:57 -0400

    Added route to create new post

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,14 @@
 const passport = require('passport')
-const router = require("express").Router()
+const router = require('express').Router()
 const mongoose = require('mongoose')
 
 const Post = require('../models/post')
 
 // Protect all router endpoints
-router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }))
+router.use(
+  '/',
+  passport.authenticate('jwt', { session: false, failWithError: true })
+)
 
 // Get all posts for user
 router.get('/', (req, res, next) => {
@@ -30,10 +33,8 @@ router.get('/:id', (req, res, next) => {
 
   Post.findOne({ userId, _id: id })
     .then(post => {
-      if (post)
-        res.json(post)
-      else
-        next()
+      if (post) res.json(post)
+      else next()
     })
     .catch(next)
 })
@@ -47,18 +48,81 @@ router.post('/', (req, res, next) => {
   for (const field of fields) {
     if (req.body[field]) {
       newPost[field] = req.body[field]
-    }
-    else {
+    } else {
       const err = new Error(`Missing ${field} in request body`)
       err.status = 400
       return next(err)
     }
   }
 
-  if (newPost.title.length < 3) {
-    
+  // Validate length of fields
+  const sizedFields = {
+    title: {
+      min: 3,
+      max: 64
+    },
+    content: {
+      min: 16
+    },
+    slug: {
+      min: 3,
+      max: 32
+    }
   }
-  if (newPost.content.length < 16) {
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+      newPost[field].trim().length < sizedFields[field].min
+  )
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+      newPost[field].trim().length > sizedFields[field].max
+  )
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    })
+  }
 
+  // Validate leading/trailing whitespace
+  for (const field of fields) {
+    if (newPost[field].length === newPost[field].trim().length) {
+      return res.status(422).json({
+        code: 422,
+        reason: 'ValidationError',
+        message: 'Must not have leading, or trailing, whitespace',
+        location: field
+      })
+    }
   }
+
+  // Validate no whitespace (slug specific)
+  if (newPost.slug.includes(' ')) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Must not contain whitespace',
+      location: 'slug'
+    })
+  }
+
+  Post.create(newPost)
+    .then(post => {
+      return res.status(201).location(`${req.originalUrl}/${post.id}`)
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Username/Title already exists')
+        err.status = 400
+      }
+      next(err)
+    })
 })
+
+module.exports = router
