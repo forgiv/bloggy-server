18c63dd620e584ba7ff3b7f6f25b02baf7756f24
Hiram Cruz <hiram0101@gmail.com>
2018-06-01 11:16:01 -0400

    added comment model

---
+++ models/comment.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const commentSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    content: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+commentSchema.set('toObject', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+    delete ret._id
+    delete ret.__v
+  }
+})
+
+module.exports = mongoose.model('Comment', commentSchema)
